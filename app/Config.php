1a5287da9b809e075d54a85748222c87fd5b62d2
mohasb <mh.haidor@gmail.com>
2022-12-04 00:18:58 +0100

    inicio

---
+++ app/Config.php
@@ -0,0 +1,8 @@
+<?php class Config
+{
+    static public $mvc_bd_hostname = "localhost";
+    static public $mvc_bd_nombre = "bdalimentos";
+    static public $mvc_bd_usuario = "root";
+    static public $mvc_bd_clave = "";
+    static public $mvc_css = "estilo.css";
+}
---
+++ web/index.php
@@ -0,0 +1,39 @@
+<?php
+// carga del modelo y los controladores
+require_once __DIR__ . '/../app/Config.php';
+require_once __DIR__ . '/../app/Model.php';
+require_once __DIR__ . '/../app/Controller.php';
+// RUTAS
+// Este array asociativo se usa para saber qué acción (función del controlador) se debe disparar
+$map = array(
+    'inicio' => array('clase' => 'Controller', 'metodo' => 'inicio'),
+    'listar' => array('clase' => 'Controller', 'metodo' => 'listar'),
+    'insertar' => array('clase' => 'Controller', 'metodo' => 'insertar'),
+    'buscarPorNombre' => array('clase' => 'Controller', 'metodo' => 'buscarPorNombre'),
+    'ver' => array('clase' => 'Controller', 'metodo' => 'ver')
+);
+// Parseo de la ruta
+if (isset($_GET['ruta'])) {
+    if (isset($map[$_GET['ruta']])) {
+        $ruta = $_GET['ruta'];
+    } else {
+        //Este error saltará si no está definida la ruta arriba en el array asociativo
+        header('Status: 404 Not Found');
+        echo '<html><body><p style="color:red"><b>ERROR: No existe la ruta ' . $_GET['ruta'] . '</b></p></body></html>';
+        exit;
+    }
+} else {
+    $ruta = 'inicio';
+}
+$controlador = $map[$ruta];
+// Ejecucion del controlador asociado a la ruta
+if (method_exists($controlador['clase'], $controlador['metodo'])) {
+
+    call_user_func(array(new $controlador['clase'], $controlador['metodo']));
+} else {
+    //Este error saltará si no está definida la función asociada a la ruta en Controller.php
+    header('Status: 404 Not Found');
+    echo '<html><body><p style="color:red"><b>ERROR: El controlador' .
+        $controlador['clase'] . '->' . $controlador['metodo'] .
+        'noexiste</b></p></body></html>';
+}
